a0645a4a59dd0561bc0f0802314893490170c915
Anshit Singh <singhanshit@gmail.com>
2018-06-01 06:16:10 -0700

    Add files via upload

---
+++ eda_imdb.py
@@ -0,0 +1,95 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+a=pd.read_csv("~/Desktop/ASS1/movie_metadata.csv")
+data=pd.DataFrame(a)
+
+data.describe(include='all')
+
+
+# In[52]:
+
+
+data.columns
+
+
+# In[3]:
+
+
+lang=data.groupby(['language'],sort=False)
+lang.describe()
+
+
+# In[4]:
+
+
+for name,group in lang:
+    print name
+    print group
+
+
+# In[54]:
+
+
+englishmovies=pd.DataFrame(lang.get_group('English'))
+print englishmovies.columns
+
+
+# In[106]:
+
+
+dir=englishmovies.groupby(['director_name'],sort=False)[['director_facebook_likes']].sum()
+a=pd.DataFrame(dir)
+
+d=a.sort_values(by=['director_facebook_likes'],ascending=False)[:10][:]
+print d
+
+
+# In[120]:
+
+
+plt.figure(figsize=(20,10))
+plt.scatter(d.index,d['director_facebook_likes'])
+plt.title('Most liked facebook directors')
+
+
+# In[139]:
+
+
+dir=englishmovies.groupby(['genres'],sort=False)[['gross']].sum()
+p=pd.DataFrame(dir)
+
+q=p.sort_values(by=['gross'],ascending=False)[:10][:]
+print q
+
+
+
+# In[141]:
+
+
+q['gross']=q['gross']/(10**9)
+q
+
+
+# In[145]:
+
+
+plt.figure(figsize=(20,10))
+plt.title("Highest grossing genres in billions of dollars")
+plt.scatter(q.index,q['gross'])
+plt.ylim(0,10)
+plt.show()
+
+
+# In[131]:
+
+
+
+
