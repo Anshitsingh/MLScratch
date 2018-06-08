86d59adbd873af45b6c4fe2275ff91b7c082640f
Anshit Singh <singhanshit@gmail.com>
2018-06-08 02:15:50 -0700

    Add files via upload

--- Convolution.py
+++ Convolution.py
@@ -13,22 +13,22 @@ image= cv2.imread("/home/jash/Desktop/dl/rubix.png")
 # In[2]:
 
 
-
 f=np.array([[[-1,0,1],[-2,0,2],[-1,0,1]],[[-1,0,1],[-2,0,2],[-1,0,1]],[[-1,0,1],[-2,0,2],[-1,0,1]]])
-
 out=np.zeros(shape=(image.shape[0],image.shape[1]))
 i_h=image.shape[0]
 i_w=image.shape[1]
 d=image.shape[2]
-
-
 f_h=f.shape[0]
 f_w=f.shape[1]
 p_h=1
 p_w=1
+output_list=[]
+s_w=2
+s_h=2
 
-p=113
-x=113*2+225
+#using formula to calculate padding
+pad=1
+x=1*2+225
 a=np.zeros(shape=(x,x,3))
 
 
@@ -36,6 +36,7 @@ a=np.zeros(shape=(x,x,3))
 
 
 def padding(image,a):
+    p=1
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
             for k in range(3):
@@ -47,32 +48,39 @@ def padding(image,a):
 # In[4]:
 
 
-
-output_list=[]
-s_w=2
-s_h=2
-
-out=np.zeros(shape=(image.shape[0],image.shape[1]))                                                                
+a=padding(image,a)
+print a.shape
 
 
 # In[5]:
 
 
-
 def convolve(image,fil):
     out=[]
-    pad=2
+    pad=1
     print image.shape
-    for i in range(i_h-p_h+1):
-        for j in range(i_w-p_w+1):
+    i_h=image.shape[0]
+    i_w=image.shape[1]
+    s_h=1
+    s_w=1
+    d=3
+    l=i_h-f_h+1
+    y=0
+    print "a shape is",a.shape
+    print "l is",l
+    for i in range(i_h-f_h+1):
+        
+        
+        for j in range(i_w-f_w+1):
             sum=0
             for k in range(d):
-                for p in range(f_h-pad):
-                    for q in range(f_w-pad):
+                for p in range(3):
+                    for q in range(3):
                         sum=sum+image[i+p][j+q][k]*f[p][q][k]
+                        
             out.append(sum)
-            j=j+s_w
-        i=i+s_h 
+        
+    print len(out)/225
     return (out)
 
 
@@ -80,44 +88,25 @@ def convolve(image,fil):
 
 
 def convert(sum,out,image):
+    print "image shape is ",image.shape
     k=0
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
             out[i][j]=sum[k]
             k=k+1
-    return out
-
-
-            
+    return out    
 
 
 # In[7]:
 
 
-a=padding(image,a)
+def sigmoid(image):
+    out=1/(1+np.exp(-1*image))
+    return out
 
 
 # In[8]:
 
-
-output_list=convolve(image,f)
-print len(output_list)
-
-
-# In[9]:
-
-
-out=convert(output_list,out,image)
-
-
-# In[10]:
-
-
-def sigmoid(image):
-    return out
-
-
-# In[11]:
 
 
 def maxpool(image):
@@ -135,11 +124,10 @@ def maxpool(image):
             j=j+1
         i=i+1
     return out
-    
-    
 
 
-# In[12]:
+# In[9]:
+
 
 
 def convertmaxpool(maxpool_list,image):
@@ -153,31 +141,27 @@ def convertmaxpool(maxpool_list,image):
     return out
 
 
-# In[13]:
+# In[ ]:
 
 
-sigmoid_output=sigmoid(out)
-print sigmoid_output
-
-
-# In[14]:
-
-
-maxpool_list=[]
-maxpool_list=maxpool(sigmoid_output)
+def forwardlayer1(a,f,image):
+    output_list=convolve(a,f)
+    out=np.zeros(shape=(image.shape[0],image.shape[1]))  
+    out=convert(output_list,out,image)
+    sigmoid_output=sigmoid(out)
+    maxpool_list=[]
+    maxpool_list=maxpool(sigmoid_output)
+    maxpool_out=convertmaxpool(maxpool_list,sigmoid_output)
+    return maxpool_out
 
 
 
 # In[ ]:
 
 
-maxpool_out=convertmaxpool(maxpool_list,sigmoid_output)
-
-
-# In[ ]:
-
-
-cv2.imshow("maxpool output",maxpool_out)
+maxpool_out=forwardlayer1(a,f,image)
+print maxpool_out.shape
+cv2.imshow("maxpooled output",maxpool_out)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
