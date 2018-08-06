d3d5dd93c477d82840bb1a833e62d97b22215d77
Anshit Singh <singhanshit@gmail.com>
2018-08-06 06:29:36 -0700

    Add files via upload

---
+++ h2o2 simple gridsearch easiest and correct almost final.py
@@ -0,0 +1,306 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+a=pd.read_table("/home/jash/Desktop/petplan/Profitability1.txt", sep='|', lineterminator='\n', error_bad_lines=False)
+
+
+# In[2]:
+
+
+data=pd.DataFrame(data=a,index=None)
+data = data.sample(frac=1).reset_index(drop=True)
+import math
+import numpy as np
+data['LossRatio'].fillna(0, inplace=True)
+data['L']=0
+data.loc[data['LossRatio'] >8.451531 ,'L'] = 1
+data.to_csv("/home/jash/Desktop/petplan/Profitability.csv")
+Y=data['L']
+y=pd.DataFrame(Y)
+x=pd.DataFrame(data)
+
+m=x.loc[x['PolicyForm']!='Introductory']
+q=pd.DataFrame(m)
+m2=q.loc[q['PolicyForm']!='Intro']
+m3=pd.DataFrame(m2)
+m4=m3.loc[m3['Quadrant']=='LowTenure-HighLR']
+x=pd.DataFrame(m4)
+
+print x.head()
+
+
+# In[3]:
+
+
+x['LossRatio'].max()
+
+
+# In[4]:
+
+
+len(x.loc[x['L']==1])
+
+
+# In[5]:
+
+
+len(x)
+
+
+# In[6]:
+
+
+
+x.isnull().sum()
+
+
+# In[7]:
+
+
+x.drop(['LossRatio'],axis=1,inplace=True)
+x.drop(['CustomerNumber','PhoneNumber','Surname','GivenName','CustomerMailingAddr_Addr1','CustomerMailingAddr_City','CustomerMailingAddr_StateProvCd',
+       'CustomerMailingAddr_PostalCode','PetId','StartDate','EndDate','LastPolicyRef'],axis=1,inplace=True)
+
+
+
+# In[8]:
+
+
+p=pd.DataFrame(x)
+
+
+# In[9]:
+
+
+p.drop(['ClaimNumber','ClaimAmount','Severity','ClaimDetails','ConditionGrp','Claimcodecategory','claimdurationInception'],axis=1,inplace=True)
+
+
+# In[10]:
+
+
+p.isnull().sum()
+
+
+# In[11]:
+
+
+
+
+p['TotalClaimsAmtPaid'].fillna(0, inplace=True)
+
+
+# In[12]:
+
+
+p.isnull().sum()
+
+
+# In[13]:
+
+
+p.dropna(how='any',inplace=True)
+
+
+# In[14]:
+
+
+p.isnull().sum()
+
+
+# In[15]:
+
+
+p.drop(['Quadrant','TotalClaimsAmtPaid','TotalEarnedPremium'],axis=1,inplace=True)
+
+
+# In[16]:
+
+
+print p.shape
+
+
+# In[17]:
+
+
+
+import h2o
+from h2o.estimators import H2ORandomForestEstimator
+h2o.init()
+df = h2o.H2OFrame(p)
+y = 'L'
+x=df.col_names
+print df
+x.remove(y)
+df[y] = df[y].asfactor()
+train, valid, test = df.split_frame(ratios=[.8,.1])
+from h2o.estimators.gbm import H2OGradientBoostingEstimator
+from h2o.grid.grid_search import H2OGridSearch
+gbm_cv3 = H2OGradientBoostingEstimator(nfolds=3,max_depth=4,ntrees=130)
+gbm_cv3.train(x=x, y=y, training_frame=train)
+## Getting all cross validated models 
+all_models = gbm_cv3.cross_validation_models()
+print("Total cross validation models: " + str(len(all_models)))
+
+
+# In[18]:
+
+
+# Use same data as above
+
+# GBM hyperparameters
+gbm_params2 = {'learn_rate': [i * 0.01 for i in range(1, 11)],
+                'max_depth': range(1,10),
+                'sample_rate': [i * 0.1 for i in range(5, 11)],
+                'col_sample_rate': [i * 0.1 for i in range(1, 11)],
+                 'min_rows':1000}
+
+# Search criteria
+search_criteria = {'strategy': 'RandomDiscrete', 'max_models': 36, 'seed': 1}
+
+# Train and validate a random grid of GBMs
+gbm_grid2 = H2OGridSearch(model=H2OGradientBoostingEstimator,
+                          grid_id='gbm_grid2',
+                          hyper_params=gbm_params2,
+                          search_criteria=search_criteria)
+gbm_grid2.train(x=x, y=y,
+                training_frame=train,
+                validation_frame=valid,
+                ntrees=100,
+                seed=1)
+
+# Get the grid results, sorted by validation AUC
+gbm_gridperf2 = gbm_grid2.get_grid(sort_by='auc', decreasing=True)
+gbm_gridperf2
+
+# Grab the top GBM model, chosen by validation AUC
+best_gbm2 = gbm_gridperf2.models[0]
+
+# Now let's evaluate the model performance on a test set
+# so we get an honest estimate of top model performance
+best_gbm_perf2 = best_gbm2.model_performance(test)
+
+best_gbm_perf2.auc()  #0.7811331652127048
+
+
+# In[33]:
+
+
+params_list = []
+for key, value in best_gbm2.params.iteritems():
+    params_list.append(str(key)+" = "+str(value['actual']))
+params_list
+
+
+# In[35]:
+
+
+best_gbm2.train(x=x, y=y, training_frame=df)
+
+
+# In[37]:
+
+
+best_gbm2.model_performance(valid)
+
+
+# In[36]:
+
+
+print best_gbm2.cross_validation_metrics_summary()
+
+
+# In[20]:
+
+
+best_gbm2.confusion_matrix()
+performance = best_gbm2.model_performance(test_data=test)
+print performance
+
+
+# In[31]:
+
+
+import matplotlib.pyplot as plt
+best_gbm2.varimp_plot()
+best_gbm2.varimp()
+
+
+# In[22]:
+
+
+import subprocess
+from IPython.display import Image
+
+
+# In[23]:
+
+
+mojo_file_name = "/home/jash/Desktop/petplan/treeshyper/my_gbm_mojo.zip"
+h2o_jar_path= '/home/jash/Desktop/petplan/trees/h2o-3.20.0.4/h2o.jar'
+mojo_full_path = mojo_file_name
+gv_file_path = "/home/jash/Desktop/petplan/treeshyper/my_gbm_graph.gv"
+
+
+# In[24]:
+
+
+image_file_name = "/home/jash/Desktop/petplan/treeshyper/my_gbm_tree_bestall_minrows1000"
+
+
+# In[25]:
+
+
+best_gbm2.download_mojo(mojo_file_name)
+
+
+# In[26]:
+
+
+
+def generateTree(h2o_jar_path, mojo_full_path, gv_file_path, image_file_path, tree_id = 0):
+    image_file_path = image_file_path + "_" + str(tree_id) + ".png"
+    result = subprocess.call(["java", "-cp", h2o_jar_path, "hex.genmodel.tools.PrintMojo", "--tree", str(tree_id), "-i", mojo_full_path , "-o", gv_file_path ], shell=False)
+    result = subprocess.call(["ls",gv_file_path], shell = False)
+    if result is 0:
+        print("Success: Graphviz file " + gv_file_path + " is generated.")
+    else: 
+        print("Error: Graphviz file " + gv_file_path + " could not be generated.")
+
+
+# In[27]:
+
+
+def generateTreeImage(gv_file_path, image_file_path, tree_id):
+    image_file_path = image_file_path + "_" + str(tree_id) + ".png"
+    result = subprocess.call(["dot", "-Tpng", gv_file_path, "-o", image_file_path], shell=False)
+    result = subprocess.call(["ls",image_file_path], shell = False)
+    if result is 0:
+        print("Success: Image File " + image_file_path + " is generated.")
+        print("Now you can execute the follow line as-it-is to see the tree graph:") 
+        print("Image(filename='" + image_file_path + "\')")
+    else:
+        print("Error: Image file " + image_file_path + " could not be generated.")
+
+
+# In[28]:
+
+
+
+generateTree(h2o_jar_path, mojo_full_path, gv_file_path, image_file_name, 0)
+
+
+# In[29]:
+
+
+generateTreeImage(gv_file_path, image_file_name, 0)
+
+
+# In[30]:
+
+
+Image(filename='/home/jash/Desktop/petplan/treeshyper/my_gbm_tree_bestall_minrows1000_0.png')
+
